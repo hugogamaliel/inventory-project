53893610b57fc1e20f75b800524ce8e713710669
hugo <hgamalielbj@gmail.com>
2020-06-17 18:09:37 -0500

    New POJO for Abono

---
+++ src/inventariojeans/Abono.java
@@ -0,0 +1,22 @@
+package inventariojeans;
+
+public class Abono 
+{
+	
+	private String idVenta;
+	private String cantidad;
+	private String fecha;
+	private int isEnganche;
+	
+	public void setIdVenta(String idVenta)
+	{
+		this.idVenta = idVenta;
+	}
+	
+	public String getIdVenta()
+	{
+		return this.idVenta;
+	}
+	
+
+}
